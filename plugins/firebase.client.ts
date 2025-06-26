d25c10297678c821893a5097b3e4f5440b7242c2
HOW YAO KANG <yaok.1h@1utar.my>
2025-06-27 00:37:06 +0800

    Firebase getDoc and RollupError

---
+++ plugins/firebase.client.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+import { defineNuxtPlugin } from '#app'
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyD_OLRFTK9D4sSH4_fTD0l4Pd-7E_FXPpc",
+    authDomain: "nailbeauty-156c9.firebaseapp.com",
+    projectId: "nailbeauty-156c9",
+    storageBucket: "nailbeauty-156c9.firebasestorage.app",
+    messagingSenderId: "756997261931",
+    appId: "1:756997261931:web:fac7a651b30e097cd0bf2e",
+    measurementId: "G-S93W6RS76Y"
+}
+    
+
